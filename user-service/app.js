5b0a6f0abbfaaed6c3bed05c014519936e819be7
Ramy Medhat <ramymedhat97@gmail.com>
2024-10-25 17:31:16 +0300

    Initial setup for user-service with Docker and MongoDB configuration

---
+++ user-service/app.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const connectDB = require("./config/dbConfig");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(morgan("dev"));
+
+// Connect to MongoDB
+
+connectDB();
+
+// Routes
+app.use("/api/users", userRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ user-service/controllers/userController.js
@@ -0,0 +1,37 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error registering user", error });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Error logging in", error });
+  }
+};
