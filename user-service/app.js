ef9aa10e12f2a988d981e0b3f90afcccd1ebd6bc
Ramy Medhat <ramymedhat97@gmail.com>
2024-10-25 18:38:18 +0300

    "Removed docker-compose.yml, added Dockerfile to product-service, and made changes to user-service files (app.js, config/dbConfig.js, controllers/userController.js, middlewares/authMiddleware.js, routes/userRoutes.js, utils/helpers.js)"

--- user-service/app.js
+++ user-service/app.js
@@ -6,12 +6,11 @@ const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
-//Middleware
+// Middleware
 app.use(express.json());
 app.use(morgan("dev"));
 
 // Connect to MongoDB
-
 connectDB();
 
 // Routes
--- user-service/controllers/userController.js
+++ user-service/controllers/userController.js
@@ -27,6 +27,7 @@ exports.login = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
       return res.status(400).json({ message: "Invalid credentials" });
+
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
