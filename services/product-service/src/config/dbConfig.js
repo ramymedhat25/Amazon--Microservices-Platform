af8cb0135e6260fd344c51aa242558602862b5a3
Ramy Medhat <ramymedhat97@gmail.com>
2024-10-27 13:02:43 +0300

    "Updated docker-compose.yml, added product-service, and refactored product-service code"

--- services/product-service/src/config/dbConfig.js
+++ services/product-service/src/config/dbConfig.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected for Product Service");
+    console.log("MongoDB connected to Product Database");
   } catch (error) {
-    console.error("MongoDB connection failed for Product Service:", error.message);
+    console.error("MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
--- services/product-service/src/models/Product.js
+++ services/product-service/src/models/Product.js
@@ -5,9 +5,8 @@ const productSchema = new mongoose.Schema(
     name: { type: String, required: true },
     description: { type: String },
     price: { type: Number, required: true },
-    category: { type: String, required: true },
-    stock: { type: Number, default: 0 },  
-    imageUrl: { type: String },           
+    category: { type: String },
+    stock: { type: Number, default: 0 },
   },
   { timestamps: true }
 );
